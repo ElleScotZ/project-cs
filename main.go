295f45323a82b92d27083c5d81679ebcd858ef04
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2023-01-29 16:41:16 +0100

    saddle**()

--- main.go
+++ main.go
@@ -989,6 +989,480 @@ func saddle5b(cp [9]surface.ControlPoint) {
 	}
 }
 
+func saddle6a(cp [8]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle6a")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func saddle6b(cp [8]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.50}},
+		{Position: [2]float64{0.50, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.50}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.50, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.50}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.50, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.50}},
+		{Position: [2]float64{0.50, 0.67}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.50, 0.33}},
+		{Position: [2]float64{0.67, 0.50}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.50, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.50}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.50, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.50}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.50, 0.33}},
+		{Position: [2]float64{0.67, 0.50}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle6b")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func saddle6c(cp [8]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.45, 0.45}},
+		{Position: [2]float64{0.55, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.45}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.45, 1.0}},
+		{Position: [2]float64{0.55, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.45}},
+		{Position: [2]float64{0.0, 0.55}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.45, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.45}},
+		{Position: [2]float64{0.45, 0.55}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.45, 0.33}},
+		{Position: [2]float64{0.55, 0.45}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.44, 0.0}},
+		{Position: [2]float64{0.55, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.45}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.55, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.45}},
+		{Position: [2]float64{1.0, 0.55}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.55, 0.45}},
+		{Position: [2]float64{0.67, 0.55}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle6c")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func saddle7a(cp [9]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[8].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.33}},
+		{Position: [2]float64{0.67, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle7a")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func saddle7b(cp [9]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[8].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.50, 0.50}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle7b")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func saddle7c(cp [9]surface.ControlPoint) {
+	var tspline surface.TSpline
+
+	cp[0].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[1].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[2].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.0, 0.67}},
+		{Position: [2]float64{0.33, 1.0}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[3].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.33}},
+		{Position: [2]float64{0.33, 0.67}},
+		{Position: [2]float64{0.67, 1.0}},
+	}
+
+	cp[4].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[5].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+	}
+
+	cp[6].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.0}},
+		{Position: [2]float64{1.0, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[7].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.33, 0.0}},
+		{Position: [2]float64{0.67, 0.33}},
+		{Position: [2]float64{1.0, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	cp[8].Knotvector = [5]surface.Knot{
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.0, 0.0}},
+		{Position: [2]float64{0.33, 0.33}},
+		{Position: [2]float64{0.67, 0.67}},
+		{Position: [2]float64{1.0, 1.0}},
+	}
+
+	tspline.ControlPoints = append(tspline.ControlPoints, cp[:]...)
+
+	err := tspline.GenerateSurface([2]int{50, 50}, "saddle7c")
+	if err != nil {
+		log.Print(err)
+	}
+}
+
 func main() {
 	// Creating control points for saddle
 	var (
@@ -1027,4 +1501,10 @@ func main() {
 	saddle4c(cp)
 	saddle5a([8]surface.ControlPoint{cp[0], cp[1], cp[2], cp[3], cp[4], cp[5], cp[6], cp[7]})
 	saddle5b(cp)
+	saddle6a([8]surface.ControlPoint{cp[0], cp[1], cp[2], cp[3], cp[4], cp[5], cp[6], cp[7]})
+	saddle6b([8]surface.ControlPoint{cp[0], cp[1], cp[2], cp[3], cp[4], cp[5], cp[6], cp[7]})
+	saddle6c([8]surface.ControlPoint{cp[0], cp[1], cp[2], cp[3], cp[4], cp[5], cp[6], cp[7]})
+	saddle7a(cp)
+	saddle7b(cp)
+	saddle7c(cp)
 }
