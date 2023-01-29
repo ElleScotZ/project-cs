d7305b2f0c96d455f1456c90d3c42a19fef440c7
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2023-01-29 17:28:14 +0100

    extra grid mesh added

--- pkg/surface/tspline.go
+++ pkg/surface/tspline.go
@@ -1,6 +1,8 @@
 package surface
 
 import (
+	"math"
+
 	"github.com/ElleScotZ/project-cs/internal/algebra"
 	"github.com/ElleScotZ/project-cs/internal/core"
 	"github.com/ElleScotZ/project-cs/pkg/io"
@@ -115,6 +117,22 @@ func (t *TSpline) GenerateSurface(resolution [2]int, fileName string) error {
 			Position: cp.Position,
 			Color:    algebra.Vector3D{Coordinates: [3]float64{150, 0, 0}},
 		})
+	}
+
+	// Creating parametric mesh
+	var parametricMesh core.Mesh
+
+	for i, cp := range t.ControlPoints {
+		for j, k := range cp.Knotvector {
+			parametricMesh.Vertices = append(parametricMesh.Vertices, core.Vertex{
+				Position: algebra.Vector3D{Coordinates: [3]float64{k.Position[0], k.Position[1], float64(i)}},
+				Color:    algebra.Vector3D{Coordinates: [3]float64{25 * float64(i), math.Abs(float64(j)-2.0) * 50, 0}},
+			})
+		}
+	}
+
+	if err := io.ExportPLY(&parametricMesh, fileName+"_grid"); err != nil {
+		return err
 	}
 
 	return io.ExportPLY(&mesh, fileName)
